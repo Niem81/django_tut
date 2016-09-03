e6319b8bb4c7e0ac5555f93236f52672131e8675
Carlos <crodriguezq91@gmail.com>
2016-09-03 00:28:32 -0500

    follwing tut

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Course
 # Create your views here.
@@ -7,5 +7,11 @@ from .models import Course
 def course_list(request):
 	courses = Course.objects.all()
 	# output = "There are " + str(len(courses)) + " articles."
-	output = ', '.join([str(course) for course in courses])
-	return HttpResponse(output)
+	# output = ', '.join([str(course) for course in courses])
+	# return HttpResponse(output)
+	return render(request, 'courses/course_list.html', {'courses': courses})
+
+def course_detail(request, pk):
+	# course = Course.objects.get(pk=pk)
+	course = get_object_or_404(Course,  pk=pk)
+	return render(request, 'courses/course_detail.html', {'course':course})
--- learning_site/views.py
+++ learning_site/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
 def hello_world(request):
-	now = datetime.datetime.now()
-	html = "<html><body>It is now %s.</body></html>" % now
-	return HttpResponse(html)
+	# now = datetime.datetime.now()
+	# html = "<html><body>It is now %s.</body></html>" % now
+	# return HttpResponse(html)
+	return render(request, 'home.html')
