c380c15a343d4a4401ac491b2e302158635f5bf7
MrK <crodriguezq91@gmail.com>
2017-04-24 08:34:04 -0500

    addind contact info

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    TOPICS = (
+		('TC', 'Tecnologia'),
+		('PR', 'Programacion'),
+		('IN', 'Innovacion'),
+		('OT', 'Others')
+	)
+    name = models.CharField()
+    email = models.EmailField()
+    message = models.TextField()
+    topic = models.CharField(choices=TOPICS)
